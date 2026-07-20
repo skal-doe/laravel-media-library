6ca0218dcdc4c4233141ed501666e651b51585f2
Latif SANGARE <latif.sangare@nourmony.ci>
2026-07-20 10:26:47 +0000

    Modification d'une variable dans la migration media_attachments

--- database/migrations/2026_07_08_121958_create_media_attachments_table.php
+++ database/migrations/2026_07_08_121958_create_media_attachments_table.php
@@ -18,7 +18,7 @@ return new class extends Migration {
             $table->string('collection_name')->default(MediaCollection::DEFAULT);
             $table->timestamps();
 
-            $table->index(['mediable_type', 'mediable_id', 'collection_name']);
+            $table->index(['mediable_type', 'mediable_id', 'collection_name'], 'media_attachments_mediable_collection_idx');
         });
     }
 
